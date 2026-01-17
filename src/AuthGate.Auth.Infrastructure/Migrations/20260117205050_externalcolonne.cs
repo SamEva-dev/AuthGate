d51a8d75d6f12a41c4cb5e0367c2c9d8e6a1919b
SamEva-dev <sam.fokam@gmail.com>
2026-01-17 22:24:09 +0100

    migration

---
+++ src/AuthGate.Auth.Infrastructure/Migrations/20260117205050_externalcolonne.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthGate.Auth.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class externalcolonne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalProvider",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalProviderId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureUrl",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalProvider",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalProviderId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureUrl",
+                table: "Users");
+        }
+    }
+}
